8bb1c41098ed8af84544e3b0bcfbcad3f41303c1
Caio Fernandes <caiovr13@gmail.com>
2023-04-05 22:24:08 -0300

    Adicionando banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,133 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _MyAppState extends State<MyApp> {
+  bool verbose = true;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  _getDatabase() async {
+    final databasePath = await getDatabasesPath();
+    final databaseLocal = join(databasePath, 'banco.db');
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    var database =
+        await openDatabase(databaseLocal, version: 1, onCreate: (db, version) {
+      String sql =
+          "CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, age INTEGER)";
+      db.execute(sql);
     });
+
+    if (kDebugMode) {
+      print("Database open: ${database.isOpen}");
+    }
+
+    return database;
+  }
+
+  _saveUser(String name, int age) async {
+    Database db = await _getDatabase();
+
+    Map<String, dynamic> userData = {"name": name, "age": age};
+
+    int id = await db.insert("users", userData);
+
+    if (kDebugMode) {
+      print("User saved. Id: $id");
+    }
+
+    return id;
+  }
+
+  _updateUser(int id, String name, int age) async {
+    Database db = await _getDatabase();
+
+    Map<String, dynamic> userData = {"name": name, "age": age};
+
+    int updatedItensCount =
+        await db.update("users", userData, where: "id = ?", whereArgs: [id]);
+
+    if (kDebugMode) {
+      print("Amount users updated: $updatedItensCount");
+    }
+
+    return updatedItensCount;
+  }
+
+  _deleteUserById(int id) async {
+    Database db = await _getDatabase();
+    int deletedItensCount =
+        await db.delete("users", where: "id = ?", whereArgs: [id]);
+
+    if (kDebugMode) {
+      print("Amount users deleted: $deletedItensCount");
+    }
+
+    return deletedItensCount;
+  }
+
+  _deleteUserByNameAndAge(String name, int age) async {
+    Database db = await _getDatabase();
+    int deletedItensCount = await db
+        .delete("users", where: "name = ? AND age = ?", whereArgs: [name, age]);
+
+    if (kDebugMode) {
+      print("Amount users deleted: $deletedItensCount");
+    }
+
+    return deletedItensCount;
+  }
+
+  _getUserById(int id) async {
+    Database db = await _getDatabase();
+
+    List users = await db.query("users",
+        columns: ["id", "name", "age"], where: "id = ?", whereArgs: [id]);
+
+    if (kDebugMode) {
+      for (var user in users) {
+        print("User id: ${user['id']}");
+        print("User name: ${user['name']}");
+        print("User age: ${user['age']}");
+      }
+    }
+
+    return users;
+  }
+
+  _getAllUsers() async {
+    Database db = await _getDatabase();
+
+    List users = await db.query("users", columns: ["id", "name", "age"]);
+
+    if (kDebugMode) {
+      for (var user in users) {
+        print("User id: ${user['id']}");
+        print("User name: ${user['name']}");
+        print("User age: ${user['age']}");
+      }
+    }
+
+    return users;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    //_saveUser("Carol Ferreira", 24);
+    //_updateUser(1, "Teste Fernandes", 0);
+    //_deleteUserById(1);
+    //_getAllUsers();
+    //_getUserById(2);
+    return Container();
   }
 }
